f217a390a2deade7ff8118db74f065822100637c
GunsooLee <laeag@naver.com>
2023-04-06 17:54:27 +0900

    first commit

---
+++ src/main/kotlin/io/knockknock/boot/springboot/vscode/springbootofvscode/SpringBootOfVsCodeApplication.kt
@@ -0,0 +1,11 @@
+package io.knockknock.boot.springboot.vscode.springbootofvscode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootOfVsCodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootOfVsCodeApplication>(*args)
+}
